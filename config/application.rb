861055d4af6c4c058d4464d055dd07a59cf9d904
Mário Nzualo <mario.nzualo@gmail.com>
2022-12-23 21:10:39 +0000

    fix config

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module RubyRailsPostgres
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << "rails-production-7c19.up.railway.app"
   end
 end
